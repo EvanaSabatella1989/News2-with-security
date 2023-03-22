0107b0adbbec18c7149b9b194b2146751c5e158c
EvanaSabatella1989 <eva.sabatella@gmail.com>
2023-03-21 21:58:11 -0300

    Admin

--- src/main/java/com/egg/News2/controladores/NoticiaControlador.java
+++ src/main/java/com/egg/News2/controladores/NoticiaControlador.java
@@ -16,54 +16,56 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/noticia")
 public class NoticiaControlador {
-    
+
     @Autowired
     private NoticiaServicio noticiaServicio;
-    
+
     @GetMapping("/")
-    public String listar(ModelMap modelo)  {
+    public String listar(ModelMap modelo) {
         List<Noticia> noticias = noticiaServicio.listarNoticias();
-        modelo.addAttribute("noticias", noticias);   
+        modelo.addAttribute("noticias", noticias);
         return "noticias.html";
     }
-    
+
     @GetMapping("/registrar") //localhost:8080/notici/registrar
-    public String registrar(){
+    public String registrar() {
         return "noticia_form.html";
     }
-    
+
     @PostMapping("/registro")
-    public String registro(@RequestParam String titulo,@RequestParam String img, @RequestParam String cuerpo, ModelMap modelo){
-        
+    public String registro(@RequestParam String titulo, @RequestParam String img, @RequestParam String cuerpo, ModelMap modelo) {
+
         try {
             noticiaServicio.crearNoticia(titulo, img, cuerpo);
-            
+
             modelo.put("exito", "La Noticia fue registrada correctamente!");
         } catch (MiException ex) {
-                       
+
             modelo.put("error", ex.getMessage());
             return "noticia_form.html";
         }
-        
-        return "index.html";        
+
+//        return "index.html";   
+        return "noticias.html";
     }
-    
+
     @GetMapping("/modificar/{id}")
     public String modificar(@PathVariable Long id, ModelMap modelo) {
-      
+
         modelo.put("noticia", noticiaServicio.getOne(id));
         return "noticia_modificar.html";
     }
-    
+
     @PostMapping("/modificar/{id}")
-    public String modificar(@PathVariable Long id, String titulo,String img, String cuerpo, ModelMap modelo) {
+    public String modificar(@PathVariable Long id, String titulo, String img, String cuerpo, ModelMap modelo) {
         try {
-           
-            noticiaServicio.modificarNoticia(id, titulo, img, cuerpo);           
-            return "redirect:../";
+
+            noticiaServicio.modificarNoticia(id, titulo, img, cuerpo);
+//            return "redirect:../";
+            return "noticias.html";
 
         } catch (MiException ex) {
-            
+
             modelo.put("error", ex.getMessage());
             return "noticia_modificar.html";
         }
